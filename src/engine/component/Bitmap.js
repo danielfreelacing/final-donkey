40dd7d1095e5935a677cf0fb5372940938533d74
danielfreelacing <danielmoratrabajo@gmail.com>
2024-02-11 22:42:36 -0800

    engine component bitmap

---
+++ src/engine/component/Bitmap.js
@@ -0,0 +1,44 @@
+import DisplayObject from "./DisplayObject";
+
+class Bitmap extends DisplayObject {
+  constructor() {
+    super();
+    this.image = null;
+    this.repeat = false;
+    /**
+     * @private
+     */
+    this.__pattern = null;
+  }
+  /**
+   * @param {Context Object} context
+   */
+  draw(context) {
+    if (this.repeat) {
+      if (!this.__pattern) {
+        this.__pattern = context.createPattern(this.image, "repeat");
+      }
+      context.fillStyle = this.__pattern;
+      context.fillRect(0, 0, this.width, this.height);
+    } else {
+      context.drawImage(
+        this.image,
+        0,
+        0,
+        this.width,
+        this.height,
+        0,
+        0,
+        this.width,
+        this.height
+      );
+    }
+    super.draw();
+  }
+  destory() {
+    this.image = null;
+    super.destory();
+  }
+}
+
+export default Bitmap;
