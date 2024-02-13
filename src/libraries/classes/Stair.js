27cf1d27634402dcd6035434f246b5931cea2053
danielfreelacing <danielmoratrabajo@gmail.com>
2024-02-12 22:48:33 -0800

    update cloud

--- src/libraries/classes/Stair.js
+++ src/libraries/classes/Stair.js
@@ -18,7 +18,7 @@ class Stair extends Sprite {
     this.x = random(10, 313);
 
     const stairTypes = [
-      "stair_friable",
+      // "stair_friable",
       "stair_moveable",
       "stair_stable_01",
       "stair_stable_02",
@@ -26,16 +26,16 @@ class Stair extends Sprite {
       "stair_stable_04",
       "stair_stable_05",
     ];
-    const name = stairTypes[random(0, 6)];
+    const name = stairTypes[random(0, 5)];
 
     if (name === "stair_moveable") {
       this.speedX = random(10, 20) / 100;
       if (random(0, 1)) {
         this.speedX = -this.speedX;
       }
-
-      this.update = this.__moveableUpdate;
     }
+
+    this.update = this.__moveableUpdate;
 
     const anim = new Animation({
       image: ImageManager.get(name),
@@ -52,14 +52,21 @@ class Stair extends Sprite {
    * @private
    */
   __moveableUpdate(deltaTime) {
-    if ((this.x < 0 && this.speedX < 0) || (this.x > 322 && this.speedX > 0)) {
-      this.speedX = -this.speedX;
+    console.log("moveupdate1");
+    if (this.name === "stair_moveable") {
+      if (
+        (this.x < 0 && this.speedX < 0) ||
+        (this.x > 322 && this.speedX > 0)
+      ) {
+        this.speedX = -this.speedX;
+      }
+
+      if (this.prop && this.lastX !== 0) {
+        this.prop.x += this.x - this.lastX;
+      }
     }
 
-    if (this.prop && this.lastX !== 0) {
-      this.prop.x += this.x - this.lastX;
-    }
-
+    this.speedY = 0.03;
     super.update(deltaTime);
   }
 }
